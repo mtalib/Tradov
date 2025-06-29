a9eb6864bbc035a6fd222df85555cdb54901b412
Mohamed Talib <mtalib4@gmail.com>
2025-06-29 21:28:19 +0100

    Updated Spyder

--- LeanAlgorithmsPython/build.sh
+++
@@ -1,9 +0,0 @@
-﻿#!/bin/bash
-mono nPython.exe -m compileall -lq ../../
-
-cp ../../__pycache__/*.pyc ../../../Launcher/bin/Debug > /dev/null
-cp ../../__pycache__/*.pyc ../../../Launcher/bin/Release > /dev/null
-cp ../../__pycache__/*.pyc ../../../Tests/bin/Debug > /dev/null
-
-# Script intentionally discards errors. The line below ensures the exit code is 0.
-exit 0
